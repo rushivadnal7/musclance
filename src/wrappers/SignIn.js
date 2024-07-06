cd2e4f30ff46fd7ef7539321e0ce39454de785e5
rushikeshtreb <rushikeshvadnal7@gmail.com>
2024-07-07 00:54:53 +0530

    message

--- src/wrappers/SignIn.js
+++ src/wrappers/SignIn.js
@@ -8,7 +8,7 @@ export const Wrapper = styled.section`
 
     .header-container{
         width: 100%;
-        height: 30%;
+        height: 25%;
         position: absolute;
         top: 0;
         display: flex;
