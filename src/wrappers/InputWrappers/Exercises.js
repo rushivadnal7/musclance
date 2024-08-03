1e5c2330ba817938e77006a2b24e1d28a2c73ce3
rushikeshtreb <rushikeshvadnal7@gmail.com>
2024-08-03 15:53:01 +0530

    message

--- src/wrappers/InputWrappers/Exercises.js
+++ src/wrappers/InputWrappers/Exercises.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: fit-content;
+    width: 100%;
     height: 250px;
     text-align: center;
     margin: 0.5rem;
     border: 2px solid var(--third-color);
     padding: 10px;
     border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     h1{
         height: 10%;
@@ -15,7 +18,7 @@ export const Wrapper = styled.div`
     }
 
     img{
-        width: 100%;
+        width: fit-content;
         height:90%;
         border-radius: 5px;
     }
